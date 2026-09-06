2742d9733084db004a5d987cc200b991429d004e
rajsekharsingh5242523 <rajsekharsingh5242523.singh@gmail.com>
2026-09-07 02:58:23 +0530

    working model

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -112,7 +112,7 @@ export default function AboutPage() {
               Interact with our live digital twin of the national grid.
             </p>
             <Link
-              href="/map-explorer"
+              href="http://localhost:8000"
               className="inline-block px-8 py-3.5 bg-gs-blue-600 hover:bg-gs-blue-500 text-white font-semibold rounded-md transition-colors"
             >
               Open Map Explorer
